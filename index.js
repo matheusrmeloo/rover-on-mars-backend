cc71ee2871361686e7cb6a810d4f97292e464669
matheusrmeloo <matheusrodrigues030717@gmail.com>
2021-03-12 15:50:30 -0300

    feat: adding txt file, chage index to use txt file

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+var fs = require("fs");
+var plateau, initPosition1, initOrientation1, commands1, initPosition2, initOrientation2, commands2;
+
+fs.readFile("./instructions.txt", function (err, data) {
+    data = data.toString();
+    let dataArr = data.split('\n');
+    plateau = dataArr[0].split('').map(Number);
+    initPosition1 = dataArr[1].split('').splice(0, 2).map(Number);
+    initOrientation1 = dataArr[1].split('').pop();
+    commands1 = dataArr[2];
+    initPosition2 = dataArr[3].split('').splice(0, 2).map(Number);
+    initOrientation2 = dataArr[3].split('').pop();
+    commands2 = dataArr[4];
+
+    console.log({
+        plateau,
+        initPosition1,
+        initOrientation1,
+        commands1,
+        initPosition2,
+        initOrientation2,
+        commands2
+    })
+});
+
+
 const CONTROLLER = {
     N: {
         x: 0,
@@ -26,14 +52,18 @@ const ORIENTATION = [
 
 class Rover {
 
-    constructor(commands, initPosition, initOrientation) {
+    constructor(commands, initPosition, initOrientation, plateau) {
+        console.log({
+            commands, initPosition, initOrientation, plateau
+        })
         this.commands = commands.split('');
         this.way = [initPosition.toString()];
         this.actualPosition = initPosition;
         this.actualOrientation = initOrientation;
+        this.plateau = plateau
     }
-    isJustOnThisWay(wayToChack) {
-        for (let way of wayToChack) {
+    isJustOnThisWay(wayToCheck) {
+        for (let way of wayToCheck) {
             if (this.way.indexOf(way) != -1) {
                 return false;
             }
@@ -68,11 +98,10 @@ class Rover {
     }
 }
 
-
-const test1 = new Rover("LMLMLMLMM", [1, 2], "N");
+const test1 = new Rover(commands1, initPosition1, initOrientation1, plateau);
 test1.move();
 
-const test2 = new Rover("MMRMMRMRRM", [3, 3], "E");
+const test2 = new Rover(commands2, initPosition2, initOrientation2, plateau);
 test2.move();
 
 if (test2.isJustOnThisWay(test1.way)) {
